8eb58c2915a18bc56d23d492d31be7253b016ca7
Morteza Javanbakht <morteza.javanbakht@gamil.com>
2022-10-18 21:32:21 +0330

    Destructuring in reduce function arguments

--- HW007/HW007-1/script.js
+++ HW007/HW007-1/script.js
@@ -1,13 +1,12 @@
 let salaries = { John: 100, Pete: 300, Mary: 250 };
 
 let findTopSalary = (inputArray) => {
-  return Object.entries(inputArray).reduce((maxEntry, entry) => {
-    let maxPerson, maxSalary, person, salary;
-    [maxPerson, maxSalary] = maxEntry;
-    [person, salary] = entry;
-    if (salary > maxSalary) return [person, salary];
-    else return [maxPerson, maxSalary];
-  });
+  return Object.entries(inputArray).reduce(
+    ([maxPerson, maxSalary], [person, salary]) => {
+      if (salary > maxSalary) return [person, salary];
+      else return [maxPerson, maxSalary];
+    }
+  );
 };
 
 console.log(findTopSalary(salaries));
