7bfb277333c2cf6a127fcd59241d50e1eac70c0f
Morteza Javanbakht <morteza.javanbakht@gamil.com>
2022-10-22 19:36:28 +0330

    Add validation for firstname and phone code and number

---
+++ HW007/HW007-6/scripts/script.js
@@ -0,0 +1,70 @@
+const firstnameInput = document.getElementById("firstname"),
+  firstnameSpan = document.getElementById("firstname_validation"),
+  phoneCodeInput = document.getElementById("phone-code"),
+  phoneCodeSpan = document.getElementById("phone-code_validation"),
+  phoneNumberInput = document.getElementById("phone-number"),
+  phoneNumberSpan = document.getElementById("phone-number_validation"),
+  emailInput = document.getElementById("email"),
+  emailSpan = document.getElementById("email_validation"),
+  passwordInput = document.getElementById("password"),
+  passwordSpan = document.getElementById("password_validation"),
+  repasswordInput = document.getElementById("repassword"),
+  repasswordSpan = document.getElementById("repassword_validation"),
+  registerBtn = document.getElementById("submit");
+
+let firstnameValidate,
+  phoneCodeValidate,
+  phoneNumberValidate,
+  emailValidate,
+  passwordValidate,
+  repasswordValidate = false;
+firstnameInput.addEventListener("input", firstnameValidation);
+phoneCodeInput.addEventListener("input", phoneCodeValidation);
+phoneNumberInput.addEventListener("input", phoneNumberValidation);
+emailInput.addEventListener("input", emailValidation);
+passwordInput.addEventListener("input", passwordValidation);
+repasswordInput.addEventListener("input", repasswordValidation);
+registerBtn.addEventListener("click", registerFunction);
+
+function firstnameValidation() {
+  if (firstnameInput.value) {
+    firstnameSpan.textContent = "✅";
+    firstnameValidate = true;
+  } else {
+    firstnameSpan.textContent = "❌";
+    firstnameValidate = false;
+  }
+}
+
+function phoneCodeValidation() {
+  let regex = /^(\+|00)[0-9]*$/;
+  if (regex.test(phoneCodeInput.value)) {
+    phoneCodeSpan.textContent = "✅";
+    phoneCodeValidation = true;
+  } else {
+    phoneCodeSpan.textContent = "❌";
+    phoneCodeValidation = false;
+  }
+}
+
+function phoneNumberValidation() {
+  let regex = /^09(0[1-2]|1[0-9]|3[0-9]|2[0-1]|9[0-9])[0-9]{3}[0-9]{4}$/;
+  if (regex.test(phoneNumberInput.value)) {
+    phoneNumberSpan.textContent = "✅";
+    phoneNumberValidation = true;
+  } else {
+    phoneNumberSpan.textContent = "❌";
+    phoneNumberValidation = false;
+  }
+}
+
+function emailValidation() {
+  let regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+  if (regex.test(emailInput.value)) {
+    emailSpan.textContent = "✅";
+    emailValidate = true;
+  } else {
+    emailSpan.textContent = "❌";
+    emailValidate = false;
+  }
+}
