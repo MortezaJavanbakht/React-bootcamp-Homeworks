3344039681a8adef876b92ca069dfcfb7ba49fe6
Morteza Javanbakht <morteza.javanbakht@gamil.com>
2022-11-10 22:39:37 +0330

    Solve first Homework

---
+++ 1/script.js
@@ -0,0 +1,10 @@
+const arr = [1,2,3,2,1];
+
+const isMaghloob = inputArray => {
+    for(let i=0; i<inputArray.length; i++){
+        if(inputArray.at(i) !== inputArray.at(-1-i)) return false;
+    }
+    return true;
+}
+
+console.log(isMaghloob(arr))
