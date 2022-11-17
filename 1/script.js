284c823906dcf094c429445b340391cef021cc18
Morteza Javanbakht <morteza.javanbakht@gamil.com>
2022-11-17 19:43:35 +0330

    Solve 1st Homework

---
+++ 1/script.js
@@ -0,0 +1,23 @@
+// 1
+const ageTable = document.getElementById("age-table")
+console.log(ageTable)
+
+//2
+const ageTableLabelElements = document.querySelectorAll("#age-table label")
+// console.log(ageTableLabelElements)
+
+//3
+const ageTableFirstTd = document.querySelector("#age-table td:first-child")
+// console.log(ageTableFirstTd)
+
+//4
+const searchForm = document.querySelector("form[name='search']")
+// console.log(searchForm)
+
+//5
+const searchFormFirstInput = document.querySelector("form[name='search'] input:first-child")
+// console.log(searchFormFirstInput)
+
+//6
+const searchFormLastInput = document.querySelector("form[name='search']>input:last-child")
+// console.log(searchFormLastInput)
