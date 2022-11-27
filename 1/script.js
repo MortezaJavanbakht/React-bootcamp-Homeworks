10a6aeb9b8ee66a2a332d541824c5416e733ee1c
Morteza Javanbakht <morteza.javanbakht@gamil.com>
2022-11-27 20:36:45 +0330

    solve part 2 form first HW

--- 1/script.js
+++ 1/script.js
@@ -15,3 +15,29 @@ class Shape {
     return 0;
   }
 }
+
+class Polygon extends Shape {
+  constructor(name, width, height) {
+    super(name);
+    this.width = width;
+    this.height = height;
+  }
+  get width() {
+    return this._width;
+  }
+  set width(value) {
+    this._width = value;
+  }
+  get height() {
+    return this._height;
+  }
+  set height(value) {
+    this._height = value;
+  }
+  getPerimeter() {
+    return this._width * 2 + this.height * 2;
+  }
+  getArea() {
+    return this._width * this._height;
+  }
+}
