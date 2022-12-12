15d0fed0ee1d4ca174eb615c2b65480da0270511
Morteza Javanbakht <morteza.javanbakht@gamil.com>
2022-12-12 23:55:53 +0330

    Add todo inserion

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,31 +1,31 @@
 const url = "https://60b77f8f17d1dc0017b8a2c4.mockapi.io/todos";
 
-export function todoRequests() {
-  function getTodos() {
-    return fetch(url)
-      .then((res) => res.json())
-      .then((result) => {
-        return result;
-      })
-      .catch((err) => {
-        return [];
-      });
-  }
-
-  function addTodo(data) {
-    return fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(data),
+function getTodos() {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((result) => {
+      return result;
     })
-      .then((res) => res.json())
-      .then((result) => {
-        return true;
-      })
-      .catch((err) => {
-        return false;
-      });
-  }
+    .catch((err) => {
+      return [];
+    });
 }
+
+function addTodo(data) {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      return true;
+    })
+    .catch((err) => {
+      return false;
+    });
+}
+
+export { addTodo, getTodos };
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,75 @@
-import { todoRequests } from "./requests.js";
+import { getTodos, addTodo } from "./requests.js";
+
+const formSubmitBtn = document.getElementById("form__submitBtn"),
+  todoTitleInput = document.getElementById("grid-title"),
+  todoDescriptionInput = document.getElementById("grid-description"),
+  todoDateInput = document.getElementById("grid-date"),
+  todoTitleError = document.getElementById("grid-title__error"),
+  todoDateError = document.getElementById("grid-date__error"),
+  toastSuccess = document.getElementById("toast__success"),
+  toastCloseBtn = document.getElementById("toast__closeBtn");
+
+formSubmitBtn.addEventListener("click", formSubmitFunc);
+todoTitleInput.addEventListener("input", RemoveTitleError);
+todoDateInput.addEventListener("input", RemoveDateError);
+toastCloseBtn.addEventListener("click", closeToast);
+
+function closeToast() {
+  toastSuccess.classList.add("hidden");
+}
+
+function formSubmitFunc() {
+  if (!todoTitleInput.value || !todoDateInput.value) {
+    if (!todoTitleInput.value) {
+      todoTitleInput.classList.add("border-red-500");
+      todoTitleError.classList.remove("hidden");
+    }
+    if (!todoDateInput.value) {
+      todoDateInput.classList.add("border-red-500");
+      todoDateError.classList.remove("hidden");
+    }
+    return;
+  }
+  generateTodo();
+}
+
+function RemoveTitleError() {
+  if (todoTitleInput.value) {
+    todoTitleInput.classList.remove("border-red-500");
+    todoTitleError.classList.add("hidden");
+  } else {
+    todoTitleInput.classList.add("border-red-500");
+    todoTitleError.classList.remove("hidden");
+  }
+}
+
+function RemoveDateError() {
+  if (todoDateInput.value) {
+    todoDateInput.classList.remove("border-red-500");
+    todoDateError.classList.add("hidden");
+  } else {
+    todoDateInput.classList.add("border-red-500");
+    todoDateError.classList.remove("hidden");
+  }
+}
+
+function generateTodo() {
+  const createTime = Date.now();
+  const toSubmitData = {
+    title: todoTitleInput.value,
+    dueDate: new Date(todoDateInput.value).getTime(),
+    description: todoDescriptionInput.value,
+    // id: Date.now().toString(36),
+    createdAt: createTime,
+    updatedAt: createTime,
+    checked: false,
+  };
+  addTodo(toSubmitData).then((res) => {
+    if (res) {
+      todoTitleInput.value = "";
+      todoDateInput.value = "";
+      todoDescriptionInput.value = "";
+      toastSuccess.classList.remove("hidden");
+    } else alert("Todo insertion failed! Please try again!");
+  });
+}
