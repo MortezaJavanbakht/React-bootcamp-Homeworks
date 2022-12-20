182d1615645ec50dc1d23debd9c1aabae17b4ba2
Morteza Javanbakht <114153047+MortezaJavanbakht@users.noreply.github.com>
2022-12-20 22:42:16 +0330

    Complete html and css

--- 1/script.js
+++ 1/script.js
@@ -1,3 +1,22 @@
+const submitBtn = document.getElementById("form__button-submit"),
+  todoTitleInput = document.getElementById("form__input-title"),
+  todoDateInput = document.getElementById("form__input-date"),
+  todoDescriptionInput = document.getElementById("form__input-description");
+
+submitBtn.addEventListener("click", addTodoFunction);
+
+function addTodoFunction() {
+  if (
+    todoTitleInput.value &&
+    todoDateInput.value &&
+    todoDescriptionInput.value
+  ) {
+    alert(todoDateInput.value);
+  } else {
+    alert("Please fill all feilds");
+  }
+}
+
 const todoApp = {
   getData: function () {
     return JSON.parse(localStorage.getItem("todoData")) ?? [];
