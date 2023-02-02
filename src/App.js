b6bdb198d9ab0711a7c9fb9f00e54f2f482be2c5
Morteza Javanbakht <114153047+MortezaJavanbakht@users.noreply.github.com>
2023-02-02 10:22:14 +0330

    complete hw018/3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [formValue, setFormValue] = useState({
+    username: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+    agreement: false,
+  });
+
+  const handleSubmitForm = (value) => {
+    value.preventDefault();
+    console.log(formValue);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="formContainer">
+        <form onSubmit={handleSubmitForm}>
+          <h2>Create account</h2>
+          <p>
+            Already have an account? <span className="link">Sign in</span>
+          </p>
+          <input
+            type="text"
+            className="username"
+            placeholder="Username"
+            onChange={(e) =>
+              setFormValue({ ...formValue, username: e.target.value })
+            }
+          />
+          <div className="inlinePart">
+            <input
+              type="text"
+              className="firstname"
+              placeholder="Firstname"
+              onChange={(e) =>
+                setFormValue({ ...formValue, firstname: e.target.value })
+              }
+            />
+            <input
+              type="text"
+              className="lastname"
+              placeholder="Lastname"
+              onChange={(e) =>
+                setFormValue({ ...formValue, lastname: e.target.value })
+              }
+            />
+          </div>
+          <input
+            type="email"
+            className="email"
+            placeholder="Email"
+            onChange={(e) =>
+              setFormValue({ ...formValue, email: e.target.value })
+            }
+          />
+          <button>
+            Sign up<span>→</span>
+          </button>
+          <div>
+            <input
+              type="checkbox"
+              name="agreement"
+              id="agreement"
+              onChange={(e) =>
+                setFormValue({ ...formValue, agreement: e.target.checked })
+              }
+            />
+            <label htmlFor="agreement">
+              I have read and agree to the{" "}
+              <span className="link">Terms of Service</span>
+            </label>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
